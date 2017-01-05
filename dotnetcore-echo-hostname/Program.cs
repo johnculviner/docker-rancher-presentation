6a2481fd2dd1628b0eb0330665108740e9212835
John Culviner <john.culviner@gmail.com>
2017-01-04 22:52:53 -0600

    added dotnet core example

---
+++ dotnetcore-echo-hostname/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleWeb
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseStartup<Startup>()
+				.UseUrls("http://0.0.0.0:8080")
+				.Build();
+
+			host.Run();
+		}
+	}
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            var message = $"Hello World from {System.Environment.MachineName} @ {DateTime.Now}";
+            app.Run(async context => await context.Response.WriteAsync(message));
+        }
+    }
+}
